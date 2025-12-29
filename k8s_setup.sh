7601f74f67b21a394de8ed9899b84bda4234a655
carlosmorette <carlos.morette@outlook.com>
2025-12-29 16:57:04 -0300

    finish k8s config

--- k8s_setup.sh
+++ k8s_setup.sh
@@ -1,6 +1,14 @@
+#!/bin/bash
+
+eval $(minikube docker-env)
+
 # Docker build command for Kubernetes deployment
 docker build --no-cache -t my_app:local .
 minikube image load my_app:local
 
 # Apply Kubernetes manifests
 kubectl apply -f k8s/
+
+
+##
+kubectl rollout restart deployment my-app
